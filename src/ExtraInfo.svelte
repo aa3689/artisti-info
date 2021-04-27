3341b3499c0706b3f02e15fdca70e63f90a06bc3
Toni Hiidenvuo <aa3689@student.jamk.fi>
2021-04-27 18:14:48 +0300

    Contains extra informations

---
+++ src/ExtraInfo.svelte
@@ -0,0 +1,58 @@
+<script>
+    import Modal from './Modal.svelte';
+    import Button from './Button.svelte';
+    import artistData from './artistData.js';
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+    const hideExtraInfo = () => dispatch('hideExtraInfo');
+
+</script>
+
+<Modal>
+    <div id="extraInfoBox">
+    <p>Perustamisvuosi: 
+        {#if $artistData.intFormedYear.length < 1}
+        Ei tiedossa
+        {:else}
+        {$artistData.intFormedYear}
+        {/if}
+    </p>
+    <p>Genre: 
+        {#if $artistData.strGenre.length < 1}
+        Ei tiedossa
+        {:else}
+        {$artistData.strGenre}
+        {/if}
+    </p>
+    <p>Jäseniä: 
+        {#if $artistData.intMembers.length < 1}
+        Ei tiedossa
+        {:else}
+        {$artistData.intMembers}
+        {/if}
+    </p>
+    <p>Kotoisin: 
+        {#if $artistData.strCountry.length < 1}
+        Ei tiedossa
+        {:else}
+        {$artistData.strCountry}
+        {/if}
+    </p>
+    </div>
+
+    <div id="closeModal">
+        <Button on:click={hideExtraInfo}>Sulje</Button>
+    </div>
+</Modal>
+
+<style>
+    #extraInfoBox {
+    font-family: 'Bookman Old Times', 'Times New Roman', serif;
+    font-size: 1.2rem;
+    }
+
+    #closeModal {
+        text-align: center;
+    }
+</style>
